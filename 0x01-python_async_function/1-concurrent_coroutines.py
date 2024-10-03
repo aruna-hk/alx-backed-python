db2b5e34b85980c3b58373c290ba0d689c74bf0d
hk <kiptooharon.hk@gmail.com>
2024-10-03 18:08:17 +0300

    sorted delays

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,13 +3,14 @@
 """
 
 import asyncio
+from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """severl coroutine
     """
     tasks = []
     for i in range(n):
         tasks.append(asyncio.ensure_future(wait_random(max_delay)))
-    return await asyncio.gather(*tasks)
+    return sorted(await asyncio.gather(*tasks))
