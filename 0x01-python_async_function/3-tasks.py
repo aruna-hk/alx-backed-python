b6f3ad2b1a9e18665f16169f90bd7942e956b769
hk <kiptooharon.hk@gmail.com>
2024-10-03 18:21:40 +0300

    asyncio.Task return

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ from typing import TypeVar
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> TypeVar('Task'):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """return task
     """
     return asyncio.create_task(wait_random(max_delay))
