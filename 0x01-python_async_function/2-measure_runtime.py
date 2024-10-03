b2ea668284267b06f36d33e831dd06983276c84f
hk <kiptooharon.hk@gmail.com>
2024-10-03 17:36:14 +0300

    anotated function and returns

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """measure asyncio
        time
     """
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,10 +3,11 @@
 """
 
 import asyncio
+from typing import TypeVar
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> TypeVar('Task'):
     """return task
     """
     return asyncio.create_task(wait_random(max_delay))
